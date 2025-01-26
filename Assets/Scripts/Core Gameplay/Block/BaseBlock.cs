79f7336fd79b37a3ee681338187c1c12652710e9
ninhnk2000 <ninhnk2000.gplaystudio@gmail.com>
2025-01-26 23:43:18 +0700

    prepare moving block logic

--- Assets/Scripts/Core Gameplay/Block/BaseBlock.cs
+++ Assets/Scripts/Core Gameplay/Block/BaseBlock.cs
@@ -3,9 +3,55 @@ using UnityEngine;
 public class BaseBlock : MonoBehaviour
 {
     [SerializeField] private BlockProperty blockProperty;
+    [SerializeField] private GameObject tilePrefab;
+
+    [SerializeField] private Rigidbody blockRigidBody;
+
+    [SerializeField] private float speedMultiplier;
 
     public BlockProperty BlockProperty
     {
         get => blockProperty;
+    }
+
+    public void Move(Vector2 inputDirection)
+    {
+        blockRigidBody.linearVelocity = speedMultiplier * new Vector3(inputDirection.x, 0, inputDirection.y);
+    }
+
+
+    public void Stop()
+    {
+        Snap();
+    }
+
+    private float GetTileDistance()
+    {
+        return 1.05f * tilePrefab.GetComponent<MeshRenderer>().bounds.size.x;
+    }
+
+    private void Snap()
+    {
+        float tileDistance = GetTileDistance();
+
+        Vector3 position = transform.position;
+
+        Vector2 coordinator;
+
+        // covert to bottom-right position
+        Vector3 bottomRightPosition = new Vector3();
+
+        bottomRightPosition.x = transform.position.x + (BlockProperty.NumTileX - 1) / 2f * tileDistance;
+        bottomRightPosition.z = transform.position.z - (BlockProperty.NumTileZ - 1) / 2f * tileDistance;
+
+        coordinator.x = Mathf.Round(bottomRightPosition.x / tileDistance);
+        coordinator.y = Mathf.Round(bottomRightPosition.z / tileDistance);
+
+        Vector3 finalPosition = new Vector3(0, 2, 0);
+
+        finalPosition.x = coordinator.x * tileDistance - (BlockProperty.NumTileX - 1) / 2f * tileDistance;
+        finalPosition.z = coordinator.y * tileDistance + (BlockProperty.NumTileZ - 1) / 2f * tileDistance;
+
+        transform.position = finalPosition;
     }
 }
---
+++ Assets/Scripts/Core Gameplay/Input/BlockSelectionInput.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using static GameEnum;
+
+public class BlockSelectionInput : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+
+    [Header("CUSTOMIZE")]
+    [SerializeField] private LayerMask layerMaskCheck;
+    [SerializeField] private float maxSwipeDistance;
+    [SerializeField] private float timeToConfirmSelectObjectPart;
+
+    #region PRIVATE FIELD
+    [SerializeField] private InputMode _inputMode;
+    private List<Vector3> twoTouchPoints;
+    private Vector3 _prevTouchPosition;
+    private BaseBlock _selectedBlock;
+    #endregion
+
+    #region EVENT
+    public static event Action mouseUpEvent;
+    public static event Action selectScrewEvent;
+    public static event Action breakObjectEvent;
+    #endregion
+
+    void Awake()
+    {
+        GameStateMachine.enableInputEvent += EnableInput;
+
+        twoTouchPoints = new List<Vector3>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        Gizmos.DrawRay(ray);
+    }
+
+    void OnDestroy()
+    {
+        GameStateMachine.enableInputEvent -= EnableInput;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SelectBlock();
+
+            MoveBlock();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _selectedBlock.Stop();
+
+            _selectedBlock = null;
+        }
+    }
+
+    private async void SelectBlock()
+    {
+        if (_selectedBlock != null)
+        {
+            return;
+        }
+
+        if (IsClickedOnUI())
+        {
+            return;
+        }
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        Physics.Raycast(ray, out RaycastHit hit, 999);
+
+        if (hit.collider != null)
+        {
+            BaseBlock block = hit.collider.GetComponent<BaseBlock>();
+
+            if (block != null)
+            {
+                _selectedBlock = block;
+
+                _prevTouchPosition = Input.mousePosition;
+            }
+        }
+    }
+
+    private void MoveBlock()
+    {
+        if (_selectedBlock != null)
+        {
+            Vector2 direction = Input.mousePosition - _prevTouchPosition;
+
+            _selectedBlock.Move(direction);
+
+            _prevTouchPosition = Input.mousePosition;
+        }
+    }
+
+    private void EnableInput(bool isEnable)
+    {
+        if (isEnable)
+        {
+            _inputMode = InputMode.Select;
+        }
+        else
+        {
+            _inputMode = InputMode.Disabled;
+        }
+    }
+
+    private bool IsClickedOnUI()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        var raycastResults = new System.Collections.Generic.List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, raycastResults);
+
+        if (raycastResults.Count > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
