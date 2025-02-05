1a88512821909e409307795f755f92c86ac3f83f
ninhnk <ninhnk2000.gplaystudio@gmail.com>
2025-02-05 17:58:05 +0700

    prepare wooden blocks

---
+++ Assets/Scripts/Core Gameplay/Special/Wooden Block/WoodenBlock.cs
@@ -0,0 +1,69 @@
+using TMPro;
+using UnityEngine;
+
+public class WoodenBlock : BaseBlock
+{
+    [Header("FREEZE BLOCK")]
+    private bool _isFreeze;
+    [SerializeField] private int remainingBlockToMelt;
+
+    [SerializeField] private GameObject ice;
+    [SerializeField] private TMP_Text remainingBlockToMeltText;
+    [SerializeField] private ParticleSystem breakIceFx;
+
+    protected override void MoreLogicInAwake()
+    {
+        blockCompletedEvent += OnBlockDisintegrated;
+
+        _isFreeze = true;
+
+        remainingBlockToMeltText.text = $"{remainingBlockToMelt}";
+    }
+
+    protected override void MoreLogicOnDestroy()
+    {
+        blockCompletedEvent -= OnBlockDisintegrated;
+    }
+
+    public override void Move(Vector3 targetPosition)
+    {
+        if (_isFreeze)
+        {
+            return;
+        }
+
+        base.Move(targetPosition);
+    }
+
+    public override void Stop()
+    {
+        if (_isFreeze)
+        {
+            return;
+        }
+
+        base.Stop();
+    }
+
+    private void OnBlockDisintegrated()
+    {
+        if (remainingBlockToMelt <= 0)
+        {
+            return;
+        }
+
+        remainingBlockToMelt--;
+
+        if (remainingBlockToMelt == 0)
+        {
+            _isFreeze = false;
+
+            remainingBlockToMeltText.gameObject.SetActive(false);
+            ice.SetActive(false);
+
+            breakIceFx.Play();
+        }
+
+        remainingBlockToMeltText.text = $"{remainingBlockToMelt}";
+    }
+}
